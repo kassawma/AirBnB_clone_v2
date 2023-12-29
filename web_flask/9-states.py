3d1683a2d781398dcd5b9361f6eb7401e6437f91
kassawma <markonayalew017@gmail.com>
2023-12-29 06:29:58 +0000

    update

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -2,18 +2,20 @@
 """
 starts a Flask web application
 """
-
 from flask import Flask, render_template
 from models import *
 from models import storage
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id=None):
     """display the states and cities listed in alphabetical order"""
-    states = storage.all("State").values()
-    return render_template('8-cities_by_states.html', states=states)
+    states = storage.all("State")
+    if state_id is not None:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
 
 
 @app.teardown_appcontext
